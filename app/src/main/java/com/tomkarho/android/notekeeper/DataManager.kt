38d5c4ddfb27832048bb85396f2dff40c00e36ae
tomkarho <tommi@tomkarho.com>
2018-11-09 12:05:20 +0200

    Add some mock data

--- app/src/main/java/com/tomkarho/android/notekeeper/DataManager.kt
+++ app/src/main/java/com/tomkarho/android/notekeeper/DataManager.kt
@@ -3,4 +3,22 @@ package com.tomkarho.android.notekeeper
 class DataManager {
     val courses = HashMap<String, CourseInfo>()
     val cotes = ArrayList<NoteInfo>()
+
+    init {
+        initializeCourses()
+    }
+
+    private fun initializeCourses() {
+        var c = CourseInfo("android_intents", "Android programming with intents")
+        courses[c.courseId] = c
+
+        c = CourseInfo(courseId = "android_async", title = "Android async programming and services")
+
+        courses[c.courseId] = c
+
+        // Notice how the order is reversed
+        c = CourseInfo(title = "Java fundementals", courseId = "java_fundementals")
+
+        courses[c.courseId] = c
+    }
 }
